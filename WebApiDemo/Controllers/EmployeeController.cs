7e44448ccebcacf67e65d8db3776a427f4296e5f
OVI <OVI@DESKTOP-DF5TG5J>
2017-08-16 02:02:55 +0600

    lecture 4

---
+++ WebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmplyeeDB;
+namespace WebApiDemo.Controllers
+{
+    [AllowAnonymous]
+    public class EmployeeController : ApiController
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+
+        [HttpGet]
+        public  IEnumerable<Employee> GetEmployeeList()
+        {
+            return db.Employees.ToList();
+        }
+
+        [HttpPost]
+        public  Employee GetEmployeeById(string Id)
+        {
+            var a = Convert.ToInt32(Id);
+            return db.Employees.Where(e => e.ID.Equals(a)).FirstOrDefault();
+        }
+
+    }
+}
